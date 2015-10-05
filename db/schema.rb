dcd10fc8784dce65dc0b9a10583f33a995d22e11
Omrika <omrikafolkes@gmail.com>
2015-10-05 16:14:23 -0400

    add name column in events

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20151005173324) do
 
   create_table "events", force: :cascade do |t|
+    t.string   "name"
     t.text     "description"
     t.string   "location"
     t.integer  "hours"
